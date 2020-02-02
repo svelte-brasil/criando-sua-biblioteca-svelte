2822dd540ab0b4fd9cc13ce7e8c4c3aba18d1847
André Lins <andrelucas01@hotmail.com>
2020-02-02 11:33:52 -0300

    Criando exemplo básico

---
+++ example/src/dev/App.svelte
@@ -0,0 +1,20 @@
+<script>
+  import SvelteErrorScreen from "../lib/index.svelte";
+</script>
+
+<style>
+  main {
+    display: flex;
+    width: 100%;
+    height: 100%;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+  }
+</style>
+
+<main>
+  <SvelteErrorScreen
+    error={500}
+    description="Ocorreu um problema no servidor" />
+</main>
---
+++ example/src/lib/index.svelte
@@ -0,0 +1,35 @@
+<script>
+  export let error;
+  export let description;
+</script>
+
+<style>
+  div {
+    display: flex;
+    width: 100%;
+    height: 100%;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    padding: 0 30%;
+    box-sizing: border-box;
+    text-align: center;
+    background-color: #eaeaea;
+    font-family: arial;
+  }
+  h2 {
+    font-size: 80pt;
+    font-weight: 700;
+    margin: 0;
+  }
+  h4 {
+    font-size: 30pt;
+    font-weight: 100;
+    margin: 0;
+  }
+</style>
+
+<div>
+  <h2>{error}</h2>
+  <h4>{description}</h4>
+</div>
